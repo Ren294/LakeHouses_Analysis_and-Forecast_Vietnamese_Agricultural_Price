6078ceba5add5e34388a325343b921bbacc5d9ac
Dung2k3 <tmdung2k3@gmail.com>
2025-05-16 20:04:08 +0700

    MD-04 | Add Predict Price pipeline to airflow

---
+++ airflow/dags/model/PredictCropPrice.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import pyspark.pandas as ps
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense, Dropout, Input
+from tensorflow.keras.layers import LSTM
+
+import keras_tuner as kt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam, RMSprop
+from datetime import datetime
+import joblib
+
+from .common import read_warehouse_pandas, write_to_hudi, create_spark_session, trino_connection, create_table
+from .config import get_all_crop_codes
+
+
+def load_crop_data(crop_code):
+    sql_query = f"""
+        SELECT  ff.ProducerPrice_LCU_tonne_LCU_month,
+                dd.dateint,
+                dd.Date,
+                ff.CropCode
+        FROM fact_faostat AS ff           
+            JOIN (SELECT DateINT,YearMonth, Date 
+                    FROM dim_date
+                    WHERE Date >= DATE'2011-01-01' 
+                        AND Date <= DATE '2023-12-01') AS dd
+                ON dd.DateINT = ff.DateINT
+        WHERE ProducerPrice_SLC_tonne_SLC_month IS NULL
+            AND ff.CropCode = {crop_code}
+        ORDER BY dd.YearMonth
+    """
+    df_pandas = pd.read_sql(sql_query, trino_connection())
+    df_pandas.interpolate(method='linear', inplace=True, limit_direction='both')
+    return df_pandas
+
+def load_models(crop_code):
+    rnn_model = tf.keras.models.load_model(f'/opt/airflow/data/models/{crop_code}/crop_{crop_code}_rnn.keras')
+    lstm_model = tf.keras.models.load_model(f'/opt/airflow/data/models/{crop_code}/crop_{crop_code}_lstm.keras')
+    scaler = joblib.load(f'/opt/airflow/data/models/{crop_code}/scaler_crop_{crop_code}.pkl')
+    return rnn_model, lstm_model, scaler
+
+
+def predict_next(crop_code, seq_length = 6, prediction_length = 24):
+    rnn_model, lstm_model, scaler = load_models(crop_code)
+
+    df = load_crop_data(crop_code)
+
+    scaled_prices = scaler.transform(df[['ProducerPrice_LCU_tonne_LCU_month']].values)
+    last_seq = scaled_prices[-seq_length:].reshape(1, seq_length, 1)
+
+    future_preds_rnn = []
+    future_preds_lstm = []
+    future_dates = []
+
+    last_dateint = df['dateint'].values[-1]
+
+    for i in range(prediction_length):
+        pred_rnn = rnn_model.predict(last_seq, verbose=0)[0][0]
+        pred_lstm = lstm_model.predict(last_seq, verbose=0)[0][0]
+
+        future_preds_rnn.append(pred_rnn)
+        future_preds_lstm.append(pred_lstm)
+
+        last_year = int(str(last_dateint)[:4])
+        last_month = int(str(last_dateint)[4:6])
+        new_month = last_month + 1
+        new_year = last_year + new_month // 13
+        new_month = new_month % 13 if new_month % 13 != 0 else 1
+
+        next_dateint = int(f"{new_year}{new_month:02d}")
+        future_dates.append(next_dateint)
+        last_dateint = next_dateint
+        last_seq = np.append(last_seq[:, 1:, :], [[[pred_lstm]]], axis=1)
+
+    future_preds_rnn = scaler.inverse_transform(np.array(future_preds_rnn).reshape(-1, 1)).reshape(-1)
+    future_preds_lstm = scaler.inverse_transform(np.array(future_preds_lstm).reshape(-1, 1)).reshape(-1)
+    result = pd.DataFrame({
+        'DateINT': future_dates,
+        'CropCode': crop_code,
+        'PredictRNN': future_preds_rnn,
+        'PredictLSTM': future_preds_lstm
+    })
+    result['timestamp'] = datetime.now()
+    result['recordId'] = result['CropCode'].astype(str) + "_" + result['DateINT'].astype(str)
+    return result
+
+def predict_crop_price(crop_code = None, path = ''):
+    if crop_code is None:
+        crop_codes = get_all_crop_codes()
+        predict_data = pd.DataFrame()
+        for code in crop_codes:
+            predict_1crops = predict_next(code, prediction_length=60)
+            predict_data = pd.concat([predict_data, predict_1crops], ignore_index=True)
+    else:
+        predict_data = predict_next(crop_code,prediction_length=60)
+    print("Started spark session")
+    spark = create_spark_session("PricePrediction")
+    df_spark = spark.createDataFrame(predict_data)
+    print("Writing to Hudi")
+    write_to_hudi(df_spark, "predict_crop_price", path, recordkey="recordId",
+                    precombine="timestamp", mode="append")
+    print("Creating table")
+    create_table(spark, "predict_crop_price", path)
+    print(f"Predict completed for CropCode {crop_code}")
+
---
+++ airflow/dags/model/common.py
@@ -0,0 +1,89 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, LongType, FloatType
+import faostat
+from pyspark.sql import DataFrame
+from .config import get_config_minio
+import trino
+s3_config = get_config_minio()
+
+
+def create_spark_session(appName):
+    warehouse_dir = "s3a://gold/hive/warehouse"
+
+    return SparkSession.builder \
+        .appName(appName) \
+        .config("spark.driver.memory", "4g") \
+        .config("spark.executor.memory", "4g") \
+        .config("spark.executor.instances", "2") \
+        .config("spark.executor.cores", "2") \
+        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+        .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.hudi.catalog.HoodieCatalog") \
+        .config("spark.kryo.registrator", "org.apache.spark.HoodieSparkKryoRegistrar") \
+        .config("spark.jars.packages",
+                "org.apache.hudi:hudi-spark3.5-bundle_2.12:0.15.0,"
+                "org.apache.hadoop:hadoop-aws:3.3.1,"
+                "com.amazonaws:aws-java-sdk-bundle:1.11.1026,"
+                "org.postgresql:postgresql:42.7.1") \
+        .config("spark.hadoop.fs.s3a.endpoint", s3_config["endpoint"]) \
+        .config("spark.hadoop.fs.s3a.access.key", s3_config["access_key"]) \
+        .config("spark.hadoop.fs.s3a.secret.key", s3_config["secret_key"]) \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider",
+                "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+        .config("spark.sql.warehouse.dir", warehouse_dir) \
+        .config("javax.jdo.option.ConnectionDriverName", "org.postgresql.Driver") \
+        .config("javax.jdo.option.ConnectionURL", "jdbc:postgresql://host.docker.internal:5431/metastore") \
+        .config("javax.jdo.option.ConnectionUserName", "postgres") \
+        .config("javax.jdo.option.ConnectionPassword", "postgres") \
+        .config("spark.sql.hive.metastore.version", "2.3.9") \
+        .config("datanucleus.autoCreateSchema", "True") \
+        .config("datanucleus.autoCreateTables", "True") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+
+def read_warehouse_pandas(spark, query):
+    return spark.sql(f"{query}").toPandas()
+
+def read_from_hudi(spark, base_path, table_name):
+    s3_path = f"{base_path}/{table_name}"
+    df = spark.read.format("hudi").load(s3_path)
+    return df
+
+def write_to_hudi(df, table_name, s3_base_path, partitionpath = None, operation="upsert", recordkey="record_id", 
+                  precombine="timestamp", mode="overwrite"):
+    
+    hudi_options = {
+        "hoodie.table.name": table_name,
+        "hoodie.datasource.write.recordkey.field": recordkey,
+        "hoodie.datasource.write.table.name": table_name,
+        "hoodie.datasource.write.operation": operation,
+        "hoodie.datasource.write.precombine.field": precombine
+    }
+    if partitionpath is not None:
+        hudi_options["hoodie.datasource.write.partitionpath.field"] = partitionpath
+
+    print(f"Writing to Hudi table: {table_name} at {s3_base_path}")
+    df.write.format("hudi") \
+        .options(**hudi_options) \
+        .mode(mode) \
+        .save(s3_base_path)
+    
+def create_table(spark, tablename, path, schema="default"):
+    spark.sql(f"""
+      CREATE TABLE IF NOT EXISTS {schema}.{tablename}
+      USING hudi
+      LOCATION '{path}'
+    """)
+
+def trino_connection():
+    return trino.dbapi.connect(
+        host='host.docker.internal',      
+        port=8080,                      
+        user='admin',
+        catalog='hive',        
+        schema='default',          
+    )   
