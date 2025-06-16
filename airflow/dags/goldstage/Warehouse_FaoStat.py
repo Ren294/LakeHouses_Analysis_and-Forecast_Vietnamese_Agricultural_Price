4675c27f3497b4f3b2e76af9941bdd016d385949
Dung2k3 <tmdung2k3@gmail.com>
2025-06-16 19:05:50 +0700

    fix | fix Rice

--- airflow/dags/goldstage/Warehouse_FaoStat.py
+++ airflow/dags/goldstage/Warehouse_FaoStat.py
@@ -25,34 +25,50 @@ def fill_rice_data(df):
         return df
 
     trade_columns_to_replace = [
-        "ExportQuantity_I_5910_I_t_I_year",
-        "ExportValue_I_5922_I_1000_USD_I_year",
-        "ImportQuantity_I_5610_I_t_I_year",
-        "ImportValue_I_5622_I_1000_USD_I_year"
+        "Exportquantity_I_5910_I_t_I_year",
+        "Exportvalue_I_5922_I_1000_USD_I_year",
+        "Importquantity_I_5610_I_t_I_year",
+        "Importvalue_I_5622_I_1000_USD_I_year"
     ]
 
     join_keys = ["AreaCode", "Area", "Year"]
     if "Months" in rice_paddy.columns:
         join_keys.append("Months")
 
-    filled_rice_paddy = rice_paddy.alias("paddy").join(
-        rice_regular.alias("regular"),
+    # filled_rice_paddy = rice_paddy.alias("paddy").join(
+    #     rice_regular.alias("regular"),
+    #     join_keys,
+    #     "left"
+    # )
+
+    filled_rice_regular = rice_regular.alias("regular").join(
+        rice_paddy.alias("paddy"),
         join_keys,
         "left"
     )
 
     select_cols = []
-    for col_name in rice_paddy.columns:
+    # for col_name in rice_paddy.columns:
+    #     if col_name in trade_columns_to_replace:
+    #         select_cols.append(
+    #             F.col(f"regular.{col_name}").alias(col_name)
+    #         )
+    #         print(f"Replacing {col_name} in rice paddy with rice regular data")
+    #     else:
+    #         select_cols.append(F.col(f"paddy.{col_name}"))
+
+    for col_name in rice_regular.columns:
         if col_name in trade_columns_to_replace:
             select_cols.append(
-                F.col(f"regular.{col_name}").alias(col_name)
+                F.col(f"paddy.{col_name}").alias(col_name)
             )
+            print(f"Replacing {col_name} in rice regular with rice paddy data")
         else:
-            select_cols.append(F.col(f"paddy.{col_name}"))
+            select_cols.append(F.col(f"regular.{col_name}"))
 
-    filled_rice_paddy = filled_rice_paddy.select(*select_cols)
+    filled_rice_regular = filled_rice_regular.select(*select_cols)
 
-    result_df = filled_rice_paddy.union(other_crops).union(rice_regular)
+    result_df = filled_rice_regular.union(other_crops)
 
     return result_df
 
@@ -110,7 +126,7 @@ def create_fact_faostat(spark, path):
         )
 
         write_to_hudi(fact_faostat, "fact_faostat", path,
-                      recordkey="recordid", precombine="DateINT")
+                     recordkey="recordid", precombine="DateINT")
 
         create_table(spark, "fact_faostat", path)
 
